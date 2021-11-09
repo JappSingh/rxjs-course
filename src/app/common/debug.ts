91e287aa13c3b65bfc6ac6ef8ab9c140f0b4ecfd
JAPPREET <jsingh@wabtec.com>
2021-11-09 18:30:15 -0500

    some comments

--- src/app/common/debug.ts
+++ src/app/common/debug.ts
@@ -1,27 +1,28 @@
-import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 export enum RxJsLoggingLevel {
-    TRACE,
-    DEBUG,
-    INFO,
-    ERROR
+  TRACE,
+  DEBUG,
+  INFO,
+  ERROR,
 }
 
 let rxjsLoggingLevel = RxJsLoggingLevel.INFO;
 
 export function setRxJsLoggingLevel(level: RxJsLoggingLevel) {
-    rxjsLoggingLevel = level;
+  rxjsLoggingLevel = level;
 }
 
-
-export const debug = (level: number, message:string) =>
-    (source: Observable<any>) => source
-        .pipe(
-            tap(val => {
-
-                if (level >= rxjsLoggingLevel) {
-                    console.log(message + ': ', val);
-                }
-            })
-        );
+// Custom oeprator:
+// Higher-order fn that takes in 2 args and rets another fn
+// that takes in source Obs as input and outputs another Obs (O/P of debug operator)
+export const debug =
+  (level: number, message: string) => (source: Observable<any>) =>
+    source.pipe(
+      tap((val) => {
+        if (level >= rxjsLoggingLevel) {
+          console.log(message + ": ", val);
+        }
+      })
+    );
